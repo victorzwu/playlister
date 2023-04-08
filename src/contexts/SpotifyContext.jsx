8f6298aee96a0ca8f8b00043615771982d0cbd22
Victor Wu <victor.wu08@gmail.com>
2023-04-08 11:39:56 -0700

    create Spotify context

---
+++ src/contexts/SpotifyContext.jsx
@@ -0,0 +1,9 @@
+import React, {useContext, useState, useEffect} from 'react'
+
+const SpotifyContext = React.createContext();
+
+function SpotifyProvider({children}) {
+  return (
+    <div>SpotifyContext</div>
+  )
+}
