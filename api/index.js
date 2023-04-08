6c2819b3a9b7ca0d6c8451344847d82b148819b4
Victor Wu <victor.wu08@gmail.com>
2023-04-08 12:01:59 -0700

    add dependencies and create GET and POST method for user information and to verify user

--- api/index.js
+++ api/index.js
@@ -1,23 +1,67 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import pkg from "@prisma/client";
 import morgan from "morgan";
 import cors from "cors";
-import { auth } from  'express-oauth2-jwt-bearer'
+import { auth } from "express-oauth2-jwt-bearer";
 
 const requireAuth = auth({
-    audience: process.env.AUTH0_AUDIENCE,
-    issuerBaseURL: process.env.AUTH0_ISSUER,
-    tokenSigningAlg: 'RS256'
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER,
+  tokenSigningAlg: "RS256",
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
+
+// get Profile information of authenticated user
+app.get("/me", requireAuth, async (req, res) => {
+  const auth0Id = req.auth.payload.sub;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      auth0Id,
+    },
   });
-  
-  const app = express();
-  
-  app.use(cors());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  app.use(morgan("dev"));
-  
-  const { PrismaClient } = pkg;
-  const prisma = new PrismaClient();
+
+  res.json(user);
+});
+
+// verify user status, if not registered in our database we will create it
+app.post("/verify-user", requireAuth, async (req, res) => {
+  const auth0Id = req.auth.payload.sub;
+  const email = req.auth.payload[`${process.env.AUTH0_AUDIENCE}/email`];
+  const name = req.auth.payload[`${process.env.AUTH0_AUDIENCE}/name`];
+
+  const user = await prisma.user.findUnique({
+    where: {
+      auth0Id,
+    },
+  });
+
+  if (user) {
+    res.json(user);
+  } else {
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        auth0Id,
+        name,
+      },
+    });
+
+    res.json(newUser);
+  }
+});
+
+app.listen(8000, () => {
+  console.log("Server running on http://localhost:8000 🎉 🚀");
+});
