4889f0c8b4ef1f198e2e1010807621c6ae8ba36e
Victor Wu <victor.wu08@gmail.com>
2023-04-08 12:21:32 -0700

    create Auth0 context for access token

---
+++ client/src/contexts/Auth0Context.js
@@ -0,0 +1,40 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Auth0Context = React.createContext();
+
+function Auth0Provider() {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+  const [accessToken, setAccessToken] = useState();
+
+  useEffect(() => {
+    const getAccessToken = async () => {
+      try {
+        const token = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+            scope: requestedScopes.join(" "),
+          },
+        });
+        setAccessToken(token);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (isAuthenticated) {
+      getAccessToken();
+    }
+  }, [getAccessTokenSilently, isAuthenticated]);
+
+  const value = { accessToken, setAccessToken };
+  return (
+    <AuthTokenContext.Provider value={value}>
+      {children}
+    </AuthTokenContext.Provider>
+  );
+}
+
+const useAuthToken = () => useContext(Auth0Context);
+
+export { useAuthToken, Auth0Provider };
